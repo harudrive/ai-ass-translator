05947d485724300ed2ace437d50b66181274c98b
harudrive <95627509+harudrive@users.noreply.github.com>
2023-07-14 15:35:38 +0800

    Update translator.ts

--- src/translator.ts
+++ src/translator.ts
@@ -146,7 +146,7 @@ export default class Translator {
                 throw new Error("Cannot get translation from OpenAI.");
             }
             if (!this.disableRateLimit) {
-                await sleep(31000); // API Rate limit
+                await sleep(61000); // API Rate limit
             }
             this.result.push(
                 ...translatedDialogs.map(
